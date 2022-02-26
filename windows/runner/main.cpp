09de9e5d4e8416e68ba511f040284c2e4e7252fe
yangheng <492238647@qq.com>
2022-02-26 14:39:51 +0800

    去除多行注释 空行

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10000, 10000);
   Win32Window::Size size(500, 610);
-  if (!window.CreateAndShow(L"loading", origin, size)) {
+  if (!window.CreateAndShow(L"copyright_gen", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
